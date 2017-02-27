9e8c1d7287dc6c9ca56060d6ec6237411d18b004
Li Zhang <li.zhang@appfolio.com>
2017-02-26 20:44:40 -0800

    lz - init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+/**
+ * Example top level component that renders a child component
+ */
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+
+/**
+ * Production webpack settings.
+ */
+module.exports = {
+  entry: [
+    'babel-polyfill', 'es6-promise/auto', 'whatwg-fetch', `${__dirname}/src/index`
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: `${path.basename(__dirname)}-bundle.js`
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: __dirname,
+        query: {
+          babelrc: false,
+          presets: [['es2015', { modules: false }], 'react', 'stage-1', 'stage-2'],
+          plugins: ['transform-object-assign']
+        }
+      },
+      {
+        test: /\.s?css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+};
